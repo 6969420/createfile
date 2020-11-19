4417a854fc6a55ffe9c7e0694e9394ccb7cb8aa7
Opilane TTHK <opilane@tthk.ee>
2020-11-19 13:06:46 +0200

    first commit!

---
+++ CreateFile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace CreateFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the file name:");
+            string userInput = Console.ReadLine();
+            CreateUserFile(userInput);
+        }
+
+        public static void CreateNewFile()
+        {
+            string rootPath = @"C:\Users\opilane\samples\playerOne";
+            string fileName = "ball.txt";
+            File.Create(Path.Combine(rootPath, fileName));
+        }
+
+        public static void CreateUserFile(string fileName)
+        {
+            string rootPath = @"C:\Users\opilane\samples\playerTwo";
+            File.Create(Path.Combine(rootPath, fileName));
+        }
+
+        public static void CreateFileWithExtensions(string fileName)
+        {
+            string rootPath = @"C:\Users\opilane\samples\playerTwo";
+            string fullfileName = $"{fileName}.txt";
+            File.Create(Path.Combine(rootPath, fullfileName));
+
+        }
+    }
+}
---
+++ DeleteFile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace DeleteFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void FileDelete()
+        {
+            string rootPath = @"C:\Users\opilane\samples\playerTwo";
+            string fileName = "ball.txt";
+
+            if(File.Exists(Path.Combine(rootPath, fileName)))
+            {
+                File.Delete(Path.Combine(rootPath, fileName));
+                Console.WriteLine("file deleted");
+            }else
+            {
+                Console.WriteLine("file not found");
+            }
+        }
+    }
+}
